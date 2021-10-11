090b23cd810d68c3623b7c1f978743a7d865880c
Deeraj Kumar <deerajkmr98@gmail.com>
2021-10-11 23:15:49 +0530

    add script to generate cats

---
+++ cat_generator/js/script.js
@@ -0,0 +1,7 @@
+//Cat Generator function
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src="images/cat.jpg";
+    div.appendChild(image);
+}
